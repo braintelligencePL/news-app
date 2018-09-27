2fbc132696a6fc12d3ee4c6c0e727723226b004a
Lukasz Sroczynski <lukasz.sroczynski@allegro.pl>
2018-09-27 10:42:23 +0200

    cleanup

--- news-service/src/test/groovy/pl/braintelligence/requirement/task/news/base/OperatingOnNewsEndpoint.groovy
+++ news-service/src/test/groovy/pl/braintelligence/requirement/task/news/base/OperatingOnNewsEndpoint.groovy
@@ -5,14 +5,14 @@ import pl.braintelligence.requirement.task.base.BaseHttpMethodsSpec
 
 trait OperatingOnNewsEndpoint implements BaseHttpMethodsSpec {
 
-    private final String LOCAL_URL = "http://localhost:12346"
+    private final String LOCAL_URL = "http://localhost:12346/"
 
     String localUrl(String path) {
         return LOCAL_URL + path
     }
 
     ResponseEntity getTopHeadlines() {
-        return get(localUrl("/news/pl/technology"), Object)
+        return get(localUrl("news/pl/technology"), Object)
     }
 
     ResponseEntity getTopHeadlines(String category, String country) {
