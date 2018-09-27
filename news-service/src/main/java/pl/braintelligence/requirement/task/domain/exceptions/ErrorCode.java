4f7c618261d9807ae0ca90c94e8aa1ce1ea3b304
Lukasz Sroczynski <lukasz.sroczynski@allegro.pl>
2018-09-27 13:01:11 +0200

    removed not needed error codes

--- news-service/src/main/java/pl/braintelligence/requirement/task/domain/exceptions/ErrorCode.java
+++ news-service/src/main/java/pl/braintelligence/requirement/task/domain/exceptions/ErrorCode.java
@@ -3,7 +3,5 @@ package pl.braintelligence.requirement.task.domain.exceptions;
 public enum ErrorCode {
 
     NONEXISTENT_COUNTRY,
-    NONEXISTENT_CATEGORY,
-    EMPTY_COUNTRY_CODE,
-    EMPTY_CATEGORY_CODE,
+    NONEXISTENT_CATEGORY
 }
