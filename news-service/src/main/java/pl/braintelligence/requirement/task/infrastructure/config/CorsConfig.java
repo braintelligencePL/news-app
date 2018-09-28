2109a84072b0c4ee273d4a5450a8257780bd8fac
Lukasz Sroczynski <lukasz.sroczynski@allegro.pl>
2018-09-28 07:39:43 +0200

    cors

--- news-service/src/main/java/pl/braintelligence/requirement/task/infrastructure/config/CorsConfig.java
+++ news-service/src/main/java/pl/braintelligence/requirement/task/infrastructure/config/CorsConfig.java
@@ -1,4 +1,12 @@
 package pl.braintelligence.requirement.task.infrastructure.config;
 
-public class CorsConfig {
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
 }
